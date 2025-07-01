f98eadc05a8ae82f03527ab507bb39cc4bfa8aa8
Madzz14Str <ffkenzo31@gmail.com>
2025-07-02 01:30:33 +0700

    apiii

---
+++ src/api/ai/gptt2v.js
@@ -0,0 +1,56 @@
+/**
+    @ ✨ Scrape GPT Text To Video
+    @ Base: https://play.google.com/store/apps/details?id=ai.video.generator.text.video
+**/
+
+const axios = require('axios');
+
+module.exports = function(app) {
+  async function gptt2vid(prompt) {
+    try {
+      const { data: k } = await axios.post('https://soli.aritek.app/txt2videov3', {
+        deviceID: Math.random().toString(16).substr(2, 8) + Math.random().toString(16).substr(2, 8),
+        prompt: prompt,
+        used: [],
+        versionCode: 51
+      }, {
+        headers: {
+          authorization: 'eyJzdWIiwsdeOiIyMzQyZmczNHJ0MzR0weMzQiLCJuYW1lIjorwiSm9objMdf0NTM0NT',
+                'content-type': 'application/json; charset=utf-8',
+                'accept-encoding': 'gzip',
+                'user-agent': 'okhttp/4.11.0'
+        }
+      });
+      const { data } = await axios.post('https://soli.aritek.app/video', {
+            keys: [k.key]
+      }, {
+        headers: {
+          authorization: 'eyJzdWIiwsdeOiIyMzQyZmczNHJ0MzR0weMzQiLCJuYW1lIjorwiSm9objMdf0NTM0NT',
+                'content-type': 'application/json; charset=utf-8',
+                'accept-encoding': 'gzip',
+                'user-agent': 'okhttp/4.11.0'
+        }
+      });
+      return data.datas[0].url;
+    } catch (error) {
+      console.error(error.message);
+      throw new Error('No result found');
+    }
+  }
+  
+  app.get('/ai/gpttovid', async (req, res) => {
+    try {
+      const { text } = req.query
+      if(!text) {
+        return res.status(400).json({ status: false, error: 'Text is required' });
+      }
+      const gpt = await gptt2vid(text);
+            res.status(200).json({
+                status: true,
+                video: gpt
+            });
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  })
+}
---
+++ src/api/search/search-npm.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+module.exports = function(app) {
+  app.get('/search/npm', async (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+      return res.status(400).json({ status: false, error: 'Query is required' });
+    }
+    try {
+      const npmmres = await axios.get(`https://registry.npmjs.org/-/v1/search?text=${encodeURIComponent(q)}&size=10`);
+      const npmres = npmmres.data.objects.map(pkg => ({
+        name: pkg.package.name,
+        version: pkg.package.version,
+        description: pkg.package.description,
+        date: pkg.package.date,
+        link: pkg.package.links.npm,
+        github: pkg.package.links.repository || null
+      }))
+      res.status(200).json({
+        status: true,
+        result: npmres
+      });
+    } catch (error) {
+      res.status(500).json({ status: false, error: error.message });
+    }
+  })
+}
