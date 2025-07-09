3ead22c824c1be86d1353d3114d6d8ed09e92273
Madzz14Str <ffkenzo31@gmail.com>
2025-07-09 16:03:00 +0700

    Madz Cihuyyyy

--- public/js/madzapi.js
+++ public/js/madzapi.js
@@ -37,23 +37,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     (async () => {
         try {
-          const reqq = await fetch("/metrics/countapirequest")
-          if(!reqq.ok) throw new Error(reqq.status)
-          const datareq = await reqq.json()
-          document.getElementById("dayreq").textContent = datareq.today
-          document.getElementById("monthreq").textContent = datareq.thisMonth
-          
+            const reqq = await fetch("/metrics/countapirequest");
+            if (!reqq.ok) throw new Error(reqq.status);
+            const datareq = await reqq.json();
+            document.getElementById("dayreq").textContent = datareq.today;
+            document.getElementById("monthreq").textContent = datareq.thisMonth;
+
             const litency = await fetch("/metrics/latency");
             if (!litency.ok) throw new Error(litency.status);
             const jsonn = await litency.json();
             const datak = jsonn.__overall ?? 0;
-            const sec = (datak / 1000).toFixed(2)
-            document.getElementById("latency").textContent =
-                `${sec}s ${datak}ms`;
+            const sec = (datak / 1000).toFixed(2);
+            document.getElementById(
+                "latency"
+            ).textContent = `${sec}s ${datak}ms`;
 
             const json = await fetch("/src/settings.json");
             if (!json.ok) throw new Error(json.status);
             const data = await json.json();
+
+            const container = document.getElementById("docapi");
+            data.categories.forEach(category => {
+                const kelompokApi = document.createElement("h2");
+                kelompokApi.textContent = category.name;
+                container.appendChild(kelompokApi);
+                category.items.forEach(api => {
+                    const apii = document.createElement("h3");
+                    apii.textContent = api.name;
+                    container.appendChild(apii);
+                });
+            });
 
             document.getElementById("namapi").textContent = data.name;
             document.getElementById("versiapi").textContent = data.version;
